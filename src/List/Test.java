ed79281eb67b67542ab67ce75dac221210c952e5
nipunalways <nipunalways@gmail.com>
2017-08-29 00:47:03 -0500

    test class

---
+++ src/List/Test.java
@@ -0,0 +1,23 @@
+package List;
+
+public class Test {
+	int x ;
+	public static void main(String[] args) {
+		int x=10;
+		final int y=20;
+		switch(x){
+		case 10:
+			System.out.println("hi");
+		case y*1:
+			System.out.println("");
+		}
+		
+
+		
+	}
+	
+	void my(){
+		System.out.println(x);
+	}
+
+}
